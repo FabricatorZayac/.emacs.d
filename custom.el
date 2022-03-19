a27a40b4b671ea2ea53f603712b720b32aaf797d
Eugene Fedchuk <fed4chuck@gmail.com>
2022-03-19 06:00:55 +0200

    Added elcord

--- custom.el
+++ custom.el
@@ -6,12 +6,13 @@
  '(custom-safe-themes
    '("8d5f22f7dfd3b2e4fc2f2da46ee71065a9474d0ac726b98f647bc3c7e39f2819" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" default))
  '(highlight-indent-guides-method 'bitmap)
+ '(js-indent-level 2)
  '(menu-bar-mode nil)
  '(package-archives
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(super-save emmet-mode vue-mode evil-surround highlight-numbers rainbow-delimiters pkgbuild-mode which-key highlight-indent-guides evil-goggles vterm undo-tree elpy treemacs-icons-dired company-tabnine company dashboard lsp-treemacs powerline-evil with-editor magit airline-themes powerline quickrun all-the-icons treemacs ## impatient-mode gruvbox-theme evil))
+   '(elcord super-save emmet-mode vue-mode evil-surround highlight-numbers rainbow-delimiters pkgbuild-mode which-key highlight-indent-guides evil-goggles vterm undo-tree elpy treemacs-icons-dired company-tabnine company dashboard lsp-treemacs powerline-evil with-editor magit airline-themes powerline quickrun all-the-icons treemacs ## impatient-mode gruvbox-theme evil))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
  '(whitespace-style
